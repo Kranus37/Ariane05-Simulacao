2f43d702337b32818c9d2efd234dfdb7f17eef2e
Kranus37 <kranus.darklord6637@proton.me>
2026-04-22 23:57:39 +0000

    Jesus me dá forças

--- corrigido/EstrategiaConversao(1).java
+++
@@ -1,9 +0,0 @@
-package corrigido;
-
-public interface EstrategiaConversao {
-
-    short converter(double valor);
-
-    boolean eValorSeguro(double valor);
-}
-
--- corrigido/ObservadorVoo.java
+++
@@ -1,7 +0,0 @@
-package corrigido;
-
-public interface ObservadorVoo {
-
-    void notificar(String evento, String detalhe);
-}
-
